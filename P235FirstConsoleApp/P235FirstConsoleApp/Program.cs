bbbc6cd07a308b2e6d90d9ee2083fa6ad5127c9c
Hamid V. Mammadov <Hamid.mammadov@code.local>
2023-05-16 17:31:56 +0400

    First Commit

---
+++ P235FirstConsoleApp/P235FirstConsoleApp/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace P235FirstConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = 98792;
+            int result = 0;
+
+            if (num % 3 == 0)
+            {
+                result = num * 2;
+                Console.WriteLine(result);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //int num1 = 456;
+            //int num2 = 879;
+
+            //if (num1 > num2)
+            //{
+            //    Console.WriteLine("num1 Boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("num2 Boyukdur");
+            //}
+
+
+
+            //var num = 15;
+            //var name = "Hello Worl P235";
+            //var check = false;
+
+            //Console.WriteLine(num);
+
+            //num += 10;
+
+            //Console.WriteLine(num);
+
+            //dynamic a = 15;
+
+            //Console.WriteLine(a);
+
+            //a = "Hello P235";
+
+            //Console.WriteLine(a);
+
+            //byte age = 95;
+
+            //sbyte temp = 127;
+
+            //float pi = 3.14f;
+
+            //decimal price = 99.99m;
+
+            //double grade = 65.64;
+
+            //char symbol = '@';
+            //symbol = '%';
+
+            //bool check1 = true;
+
+            //string surname = "Hello P235";
+
+            //if (num > 50)
+            //{
+            //    Console.WriteLine("50-den boyukdur");
+            //}
+            //else if (num > 40)
+            //{
+            //    Console.WriteLine("40-den boyukdur");
+            //}
+            //else if (num > 30)
+            //{
+            //    Console.WriteLine("30-den boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("30-da Kicikdir");
+            //}
+
+
+
+        }
+    }
+}
